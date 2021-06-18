03a5cd1142c18640e4b8f3597218bd46dd15b741
niikoolaa04 <nikolajovicic.business@gmail.com>
2021-06-18 08:34:00 +0200

    Fix

--- commands/giveaways/gcreate.js
+++ commands/giveaways/gcreate.js
@@ -86,7 +86,7 @@ module.exports = class GiveawayCreate extends Command {
       message.guild.id, 
       0, 
       ms(durationArg), 
-      channelArg, 
+      channelArg.id, 
       parseInt(winnersArg), 
       parseInt(messagesArg), 
       parseInt(invitesArg), 
@@ -96,6 +96,6 @@ module.exports = class GiveawayCreate extends Command {
     );
     this.client.gw.startGiveaway(this.client, message, giveawayObject);
     
-    message.channel.send(this.client.embedBuilder(this.client, message, "Giveaway", `Giveaway has started in <#${channelArg}>.`, "YELLOW"));
+    message.channel.send(this.client.embedBuilder(this.client, message, "Giveaway", `Giveaway has started in ${channelArg}.`, "YELLOW"));
   }
 };

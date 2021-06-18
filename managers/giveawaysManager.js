0c3242ebfaeb5d863e7a3cd7fe72c120db4db57b
niikoolaa04 <nikolajovicic.business@gmail.com>
2021-06-18 15:56:42 +0200

    PrettyMS

--- managers/giveawaysManager.js
+++ managers/giveawaysManager.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
-const ms = require("ms");
+const ms = require("pretty-ms");
 const random = require("random");
 const delay = require("delay");
 

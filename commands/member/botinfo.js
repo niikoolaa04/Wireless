090d545476ae270eee8d58c97944f17549ecbb43
niikoolaa04 <nikolajovicic.business@gmail.com>
2021-06-19 10:25:52 +0200

    Bot Info Command

---
+++ commands/member/botinfo.js
@@ -0,0 +1,31 @@
+const Command = require("../../structures/Command");
+const Discord = require("discord.js");
+
+module.exports = class Botinfo extends Command {
+  constructor(client) {
+    super(client, {
+      name: "botinfo",
+      description: "informations about bot",
+      usage: "botinfo",
+      permissions: [],
+      category: "utility",
+      listed: true,
+    });
+  }
+
+  async run(message, args) {
+    const memory = process.memoryUsage().heapUsed / 1024 / 1024;
+    const cpu = process.cpuUsage().system / 1024 / 1024;
+    let embed = new Discord.MessageEmbed()
+      .setAuthor("Bot Informations", this.client.user.displayAvatarURL())
+      .setDescription(`\`👤\` Developer: \`${this.client.config.dev.name}\`
+\`🤖\` Bot Version: \`${this.client.config.version}\`
+\`📚\` Programming Language: \`JavaScript\`
+\`🎮\` Library: \`discord.js\`
+\`🖥️\` Memory: \`${memory.toFixed(2)}MB\`
+\`📟\` CPU: \`${Math.round(cpu)}%\``)
+      .setColor("BLURPLE");
+  
+    message.channel.send(embed);
+  }
+};

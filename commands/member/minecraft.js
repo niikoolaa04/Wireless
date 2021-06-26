63a49271944b781bb5c3dcfe9636ba2982287cea
niikoolaa04 <nikolajovicic.business@gmail.com>
2021-06-26 18:26:43 +0200

    Minecraft Command

---
+++ commands/member/minecraft.js
@@ -0,0 +1,38 @@
+const Command = require("../../structures/Command");
+const Discord = require("discord.js");
+
+module.exports = class Minecraft extends Command {
+  constructor(client) {
+    super(client, {
+      name: "minecraft",
+      description: "minecraft achievement with your text",
+      usage: "minecraft [Tekst]",
+      aliases: ["mc", "achievement", "ach", "mcach"],
+      permissions: [],
+      category: "member",
+      listed: true,
+    });
+  }
+
+  async run(message, args) {
+    var text = args.join(' ')
+    if (text.toLowerCase().includes("ž") ||
+      text.toLowerCase().includes("č") ||
+      text.toLowerCase().includes("ć") ||
+      text.toLowerCase().includes("š") ||
+      text.toLowerCase().includes("đ")) return message.channel.send("> Instead of **'Š, Đ, Č, Ć, Ž'** use **'S, Dj, C, C, Z'**");
+
+    let embedError = new Discord.MessageEmbed()
+      .setAuthor("Error", this.client.user.displayAvatarURL())
+      .setDescription(`You need to enter text.`)
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL({ size: 1024, dynamic: true }))
+      .setColor("RANDOM");
+
+    var url = "https://minecraftskinstealer.com/achievement/" + (Math.floor(Math.random() * 30) + 1) + "/Achievement+Get%21/" + text;
+    let file = new Discord.MessageAttachment(await url, "MinecraftAchievement.png");
+
+    if (!text) return message.channel.send(embedError);
+    message.channel.send(file);
+  }
+};

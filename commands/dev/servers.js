145bb7ad6f411e73a369c9449e6a3c4bbb83b901
niikoolaa04 <nikola04jovicic@gmail.com>
2021-08-05 22:27:59 +0200

    Server list

---
+++ commands/dev/servers.js
@@ -0,0 +1,32 @@
+const Command = require("../../structures/Command");
+
+module.exports = class Servers extends Command {
+  constructor(client) {
+    super(client, {
+      name: "servers",
+      description: "list of all servers bot is in",
+      usage: "servers",
+      permissions: [],
+      aliases: ["serverlist", "serverslist"],
+      category: "dev",
+      listed: false,
+    });
+  } 
+  
+  async run(message, args) {
+    var allowedToUse = false;
+  
+    this.client.dev_ids.forEach(id => {
+      if (message.author.id == id) allowedToUse = true;
+    });
+  
+    if (allowedToUse) {
+  
+      let guilds = this.client.guilds.cache.filter(g => g.memberCount > 2)
+        .map(r => "> `" + r.name + "`" + " - **" + r.memberCount + "** members " + "[`" + r.id + "`]").join("\n");
+      message.channel.send({ content: guilds});
+    } else {
+      message.channel.send({ content: "> Only Developer can use this Command!" });
+    }
+  }
+};

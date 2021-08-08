11de189ad7e1f68e03c9e93ec45b575d145a2993
niikoolaa04 <nikola04jovicic@gmail.com>
2021-08-08 13:20:38 +0200

    Slash Commands Globaly

--- events/client/ready.js
+++ events/client/ready.js
@@ -39,6 +39,7 @@ module.exports = class Ready extends Event {
       });
     }); 
 
-    await this.client.guilds.cache.get("825090904359960586").commands.set(this.client.slashArray);
+    // await this.client.guilds.cache.get("825090904359960586").commands.set(this.client.slashArray);
+    await client.application.commands.set(this.client.slashArray);
 	}
 };

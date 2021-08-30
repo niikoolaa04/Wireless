4a4bba2d6d3db905b313966807e912cbedd6e019
niikoolaa04 <nikolajovicic.business@gmail.com>
2021-08-30 20:23:08 +0200

    Slash

--- commands/dev/test.js
+++ commands/dev/test.js
@@ -11,6 +11,28 @@ module.exports = class Test extends Command {
       permissions: [],
       category: "dev",
       listed: false,
+      slash: true, 
+      options: [{
+        name: 'osmak',
+        type: 'SUB_COMMAND',
+        description: "Test 1 ",
+        required: false,
+      },{
+        name: 'devetak',
+        type: 'SUB_COMMAND',
+        description: "Test 2",
+        required: false,
+      },{
+        name: 'desetak',
+        type: 'SUB_COMMAND',
+        description: "Test 3",
+        required: false,
+        options: [{
+          name: 'subdesetak', 
+          type: 'STRING', 
+          required: false
+        }]
+      }]
     });
   }
 
@@ -20,5 +42,17 @@ module.exports = class Test extends Command {
       if (message.author.id == id) allowedToUse = true;
     });
     if (!allowedToUse) return;
+  }
+  async slashRun(interaction, args) {
+    var allowedToUse = false;
+    this.client.dev_ids.forEach(id => {
+      if (interaction.user.id == id) allowedToUse = true;
+    });
+    if (!allowedToUse) return;
+    const [devetak, subdesetak] = args;
+    console.log("-----------")
+    console.log(subdesetak)
+    console.log(devetak);
+    console.log("-----------")
   }
 };
--- events/message/interactionCreate.js
+++ events/message/interactionCreate.js
@@ -26,6 +26,14 @@ module.exports = class InteractionCreate extends Event {
       if(userPerms.length > 0) return interaction.followUp({ embeds: [ this.client.embedInteraction(this.client, interaction, "Error", "You don't have permission to run this command.", "RED")] });
 
       const args = [];
+      for (let option of interaction.options.data) {
+        if (option.type === "SUB_COMMAND") {
+          if (option.name) args.push(option.name);
+          option.options?.forEach((x) => {
+            if (x.value) args.push(x.value);
+          });
+        } else if (option.value) args.push(option.value);
+      }
 
       cmd.slashRun(interaction, args);
     }
